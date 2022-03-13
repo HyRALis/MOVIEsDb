2603a5e4789eba5ae9cdd3a2b70c7adb21657322
HyRALis <petar.trajanoski.pt@gmail.com>
2022-03-13 17:14:02 +0100

    Heavy refactor of credits

--- src/Components/Info/Credits.jsx
+++ src/Components/Info/Credits.jsx
@@ -1,5 +1,5 @@
-import React, { useState, lazy, Suspense } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, lazy, Suspense, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 import "./Credits.scss";
 
@@ -7,46 +7,51 @@ import Spinner from "../Spinner";
 const Card = lazy(() => import("./Card"));
 
 export default function Credits({ ImageBuilder }) {
-  const dispatch = useDispatch();
   const credits = useSelector((state) => state.details.itemCredits);
-  const [option, setOption] = useState("Cast");
+  const [option, setOption] = useState("cast");
+  const [cardsToDisplay, setCardsToDisplay] = useState([]);
+
+  const creditsToDisplay = (creditsProperty) => {
+    if (credits[creditsProperty] && credits[creditsProperty].length) {
+      return credits[creditsProperty];
+    } else {
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    setCardsToDisplay(creditsToDisplay(option));
+  }, [option, credits]);
 
   return (
     <div className="credits">
       <div className="credits-nav">
         <button
-          className={option === "Cast" ? "active btn-credits" : "btn-credits"}
-          onClick={() => setOption("Cast")}
+          className={option === "cast" ? "active btn-credits" : "btn-credits"}
+          onClick={() => setOption("cast")}
         >
           Cast
         </button>
-        {credits.crew !== undefined && (
-          <button
-            className={option === "Crew" ? "active btn-credits" : "btn-credits"}
-            onClick={(e) => setOption("Crew")}
-          >
-            Crew
-          </button>
-        )}
+        {
+          (credits.crew &&
+          credits.crew.length !== 0) &&
+          (
+            <button
+              className={option === "crew" ? "active btn-credits" : "btn-credits"}
+              onClick={(e) => setOption("crew")}
+            >
+              Crew
+            </button>
+          )
+        }
       </div>
-      {credits !== undefined && (
-        <div className="credits-main">
-          {option === "Crew" &&
-            credits.crew !== undefined &&
-            credits.crew.map((item, index) => (
-              <Suspense key={item.id} fallback={<Spinner />}>
-                <Card key={index} Content={item} ImageBuilder={ImageBuilder} />
-              </Suspense>
-            ))}
-          {option === "Cast" &&
-            credits.cast !== undefined &&
-            credits.cast.map((item, index) => (
-              <Suspense key={item.id} fallback={<Spinner />}>
-                <Card key={index} Content={item} ImageBuilder={ImageBuilder} />
-              </Suspense>
-            ))}
-        </div>
-      )}
+      <div className="credits-main">
+        {cardsToDisplay.map((item, index) => (
+          <Suspense key={item.id} fallback={<Spinner />}>
+            <Card key={index} Content={item} ImageBuilder={ImageBuilder} />
+          </Suspense>))
+        }
+      </div>
     </div>
   );
 }
